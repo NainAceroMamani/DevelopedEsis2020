6d9776f8b062c3f40366fae4adcd579342f5cbdd
Nain Acero <a_nacerom@unjbg.edu.com>
2020-03-03 20:51:36 -0500

    Beneficios Inscripcion

--- resources/views/layouts/vista.blade.php
+++ resources/views/layouts/vista.blade.php
@@ -1,7 +1,7 @@
 
 @include('includes.header')            
 
-    <div class="mh-100" style="height:55px"></div>
+    <div class="mh-100" style="height:65px"></div>
 
     @include('includes.section')
 
